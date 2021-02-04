3ee3c5d2988b88c31438ca65395910371334dc1d
Jens Behmenburg <jensbehmenburg@gmail.com>
2021-02-04 17:38:04 +0100

    Added dynamic resultchange and calculation with dots (doubles)

--- app/src/main/java/com/claculator/CalculatorActivity.java
+++ app/src/main/java/com/claculator/CalculatorActivity.java
@@ -8,56 +8,20 @@ import android.widget.ViewFlipper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.math.BigDecimal;
 import java.util.Stack;
 
 public class CalculatorActivity extends AppCompatActivity {
 
     private StringBuilder calc = new StringBuilder();
     char[] operation = {'+', '-', '×', '/'};
-    char[] numbers = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+    char[] numbers = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'};
+    private boolean equalsWasPressed;
+    private boolean clearWhenNextIsNumber;
 
     private TextView calcText;
     private TextView resultText;
     private ViewFlipper vf;
-    private Button one;
-    private Button two;
-    private Button three;
-    private Button four;
-    private Button five;
-    private Button six;
-    private Button seven;
-    private Button eight;
-    private Button nine;
-    private Button zero;
-    private Button plus;
-    private Button minus;
-    private Button multiply;
-    private Button divide;
-    private Button clear;
-    private Button delete;
-    private Button equal;
-    private Button percentage;
-    private Button change;
-
-    private Button one2;
-    private Button two2;
-    private Button three2;
-    private Button four2;
-    private Button five2;
-    private Button six2;
-    private Button seven2;
-    private Button eight2;
-    private Button nine2;
-    private Button zero2;
-    private Button plus2;
-    private Button minus2;
-    private Button multiply2;
-    private Button divide2;
-    private Button clear2;
-    private Button delete2;
-    private Button equal2;
-    private Button percentage2;
-    private Button change2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,50 +30,52 @@ public class CalculatorActivity extends AppCompatActivity {
 
         calcText = findViewById(R.id.tf_calc);
         resultText = findViewById(R.id.tf_result);
-        vf = (ViewFlipper) findViewById(R.id.vf_button);
+        vf = findViewById(R.id.vf_button);
 
         calcText.setText("");
         resultText.setText("");
 
-        one = (Button) findViewById(R.id.button_One);
-        two = (Button) findViewById(R.id.button_Two);
-        three = (Button) findViewById(R.id.button_Three);
-        four = (Button) findViewById(R.id.button_Four);
-        five = (Button) findViewById(R.id.button_Five);
-        six = (Button) findViewById(R.id.button_Six);
-        seven = (Button) findViewById(R.id.button_Seven);
-        eight = (Button) findViewById(R.id.button_Eight);
-        nine = (Button) findViewById(R.id.button_Nine);
-        zero = (Button) findViewById(R.id.button_Zero);
-        plus = (Button) findViewById(R.id.button_Plus);
-        minus = (Button) findViewById(R.id.button_Minus);
-        multiply = (Button) findViewById(R.id.button_Multiply);
-        divide = (Button) findViewById(R.id.button_Divide);
-        percentage = (Button) findViewById(R.id.button_Percentage);
-        equal = (Button) findViewById(R.id.button_Equal);
-        clear = (Button) findViewById(R.id.button_Clear);
-        delete = (Button) findViewById(R.id.button_Delete);
-        change = (Button) findViewById(R.id.button_Change);
+        Button one = findViewById(R.id.button_One);
+        Button two = findViewById(R.id.button_Two);
+        Button three = findViewById(R.id.button_Three);
+        Button four = findViewById(R.id.button_Four);
+        Button five = findViewById(R.id.button_Five);
+        Button six = findViewById(R.id.button_Six);
+        Button seven = findViewById(R.id.button_Seven);
+        Button eight = findViewById(R.id.button_Eight);
+        Button nine = findViewById(R.id.button_Nine);
+        Button zero = findViewById(R.id.button_Zero);
+        Button plus = findViewById(R.id.button_Plus);
+        Button minus = findViewById(R.id.button_Minus);
+        Button multiply = findViewById(R.id.button_Multiply);
+        Button divide = findViewById(R.id.button_Divide);
+        Button percentage = findViewById(R.id.button_Percentage);
+        Button equal = findViewById(R.id.button_Equal);
+        Button dot = findViewById(R.id.button_Dot);
+        Button clear = findViewById(R.id.button_Clear);
+        Button delete = findViewById(R.id.button_Delete);
+        Button change = findViewById(R.id.button_Change);
 
-        one2 = (Button) findViewById(R.id.button_One2);
-        two2 = (Button) findViewById(R.id.button_Two2);
-        three2 = (Button) findViewById(R.id.button_Three2);
-        four2 = (Button) findViewById(R.id.button_Four2);
-        five2 = (Button) findViewById(R.id.button_Five2);
-        six2 = (Button) findViewById(R.id.button_Six2);
-        seven2 = (Button) findViewById(R.id.button_Seven2);
-        eight2 = (Button) findViewById(R.id.button_Eight2);
-        nine2 = (Button) findViewById(R.id.button_Nine2);
-        zero2 = (Button) findViewById(R.id.button_Zero2);
-        plus2 = (Button) findViewById(R.id.button_Plus2);
-        minus2 = (Button) findViewById(R.id.button_Minus2);
-        multiply2 = (Button) findViewById(R.id.button_Multiply2);
-        divide2 = (Button) findViewById(R.id.button_Divide2);
-        percentage2 = (Button) findViewById(R.id.button_Percentage2);
-        equal2 = (Button) findViewById(R.id.button_Equal2);
-        clear2 = (Button) findViewById(R.id.button_Clear2);
-        delete2 = (Button) findViewById(R.id.button_Delete2);
-        change2 = (Button) findViewById(R.id.button_Change2);
+        Button one2 = findViewById(R.id.button_One2);
+        Button two2 = findViewById(R.id.button_Two2);
+        Button three2 = findViewById(R.id.button_Three2);
+        Button four2 = findViewById(R.id.button_Four2);
+        Button five2 = findViewById(R.id.button_Five2);
+        Button six2 = findViewById(R.id.button_Six2);
+        Button seven2 = findViewById(R.id.button_Seven2);
+        Button eight2 = findViewById(R.id.button_Eight2);
+        Button nine2 = findViewById(R.id.button_Nine2);
+        Button zero2 = findViewById(R.id.button_Zero2);
+        Button plus2 = findViewById(R.id.button_Plus2);
+        Button minus2 = findViewById(R.id.button_Minus2);
+        Button multiply2 = findViewById(R.id.button_Multiply2);
+        Button divide2 = findViewById(R.id.button_Divide2);
+        Button percentage2 = findViewById(R.id.button_Percentage2);
+        Button equal2 = findViewById(R.id.button_Equal2);
+        Button dot2 = findViewById(R.id.button_Dot2);
+        Button clear2 = findViewById(R.id.button_Clear2);
+        Button delete2 = findViewById(R.id.button_Delete2);
+        Button change2 = findViewById(R.id.button_Change2);
 
 
         one.setOnClickListener(this::onOneClick);
@@ -160,6 +126,9 @@ public class CalculatorActivity extends AppCompatActivity {
         equal.setOnClickListener(this::onEqualClick);
         equal2.setOnClickListener(this::onEqualClick);
 
+        dot.setOnClickListener(this::onDotClick);
+        dot2.setOnClickListener(this::onDotClick);
+
         clear.setOnClickListener(this::onClearClick);
         clear2.setOnClickListener(this::onClearClick);
 
@@ -172,112 +141,184 @@ public class CalculatorActivity extends AppCompatActivity {
 
     private void onOneClick(View view) {
         if (calc.length() == 0 || contains(calc.charAt(calc.length() - 1), operation) || contains(calc.charAt(calc.length() - 1), numbers)) {
+            if (clearWhenNextIsNumber) {
+                calc.setLength(0);
+                clearWhenNextIsNumber = false;
+            }
+
             calc.append("1");
-            updateCalc();
+            updateCalcText();
+            updateResult();
         }
     }
 
     private void onTwoClick(View view) {
         if (calc.length() == 0 || contains(calc.charAt(calc.length() - 1), operation) || contains(calc.charAt(calc.length() - 1), numbers)) {
+            if (clearWhenNextIsNumber) {
+                calc.setLength(0);
+                clearWhenNextIsNumber = false;
+            }
+
             calc.append("2");
-            updateCalc();
+            updateCalcText();
+            updateResult();
         }
     }
 
     private void onThreeClick(View view) {
         if (calc.length() == 0 || contains(calc.charAt(calc.length() - 1), operation) || contains(calc.charAt(calc.length() - 1), numbers)) {
+            if (clearWhenNextIsNumber) {
+                calc.setLength(0);
+                clearWhenNextIsNumber = false;
+            }
+
             calc.append("3");
-            updateCalc();
+            updateCalcText();
+            updateResult();
         }
     }
 
     private void onFourClick(View view) {
         if (calc.length() == 0 || contains(calc.charAt(calc.length() - 1), operation) || contains(calc.charAt(calc.length() - 1), numbers)) {
+            if (clearWhenNextIsNumber) {
+                calc.setLength(0);
+                clearWhenNextIsNumber = false;
+            }
+
             calc.append("4");
-            updateCalc();
+            updateCalcText();
+            updateResult();
         }
     }
 
     private void onFiveClick(View view) {
         if (calc.length() == 0 || contains(calc.charAt(calc.length() - 1), operation) || contains(calc.charAt(calc.length() - 1), numbers)) {
+            if (clearWhenNextIsNumber) {
+                calc.setLength(0);
+                clearWhenNextIsNumber = false;
+            }
+
             calc.append("5");
-            updateCalc();
+            updateCalcText();
+            updateResult();
         }
     }
 
     private void onSixClick(View view) {
         if (calc.length() == 0 || contains(calc.charAt(calc.length() - 1), operation) || contains(calc.charAt(calc.length() - 1), numbers)) {
+            if (clearWhenNextIsNumber) {
+                calc.setLength(0);
+                clearWhenNextIsNumber = false;
+            }
+
             calc.append("6");
-            updateCalc();
+            updateCalcText();
+            updateResult();
         }
     }
 
     private void onSevenClick(View view) {
         if (calc.length() == 0 || contains(calc.charAt(calc.length() - 1), operation) || contains(calc.charAt(calc.length() - 1), numbers)) {
+            if (clearWhenNextIsNumber) {
+                calc.setLength(0);
+                clearWhenNextIsNumber = false;
+            }
+
             calc.append("7");
-            updateCalc();
+            updateCalcText();
+            updateResult();
         }
     }
 
     private void onEightClick(View view) {
         if (calc.length() == 0 || contains(calc.charAt(calc.length() - 1), operation) || contains(calc.charAt(calc.length() - 1), numbers)) {
+            if (clearWhenNextIsNumber) {
+                calc.setLength(0);
+                clearWhenNextIsNumber = false;
+            }
+
             calc.append("8");
-            updateCalc();
+            updateCalcText();
+            updateResult();
         }
     }
 
     private void onNineClick(View view) {
         if (calc.length() == 0 || contains(calc.charAt(calc.length() - 1), operation) || contains(calc.charAt(calc.length() - 1), numbers)) {
+            if (clearWhenNextIsNumber) {
+                calc.setLength(0);
+                clearWhenNextIsNumber = false;
+            }
+
             calc.append("9");
-            updateCalc();
+            updateCalcText();
+            updateResult();
         }
     }
 
     private void onZeroClick(View view) {
         if (calc.length() == 0 || contains(calc.charAt(calc.length() - 1), operation) || contains(calc.charAt(calc.length() - 1), numbers)) {
+            if (clearWhenNextIsNumber) {
+                calc.setLength(0);
+                clearWhenNextIsNumber = false;
+            }
+
             calc.append("0");
-            updateCalc();
+            updateCalcText();
+            updateResult();
         }
     }
 
     private void onPlusClick(View view) {
         if (calc.length() != 0 && !contains(calc.charAt(calc.length() - 1), operation)) {
             calc.append("+");
-            updateCalc();
+            clearWhenNextIsNumber = false;
+            updateCalcText();
         }
     }
 
     private void onMinusClick(View view) {
         if (calc.length() != 0 && !contains(calc.charAt(calc.length() - 1), operation)) {
             calc.append("-");
-            updateCalc();
+            clearWhenNextIsNumber = false;
+            updateCalcText();
         }
     }
 
     private void onMultiplyClick(View view) {
         if (calc.length() != 0 && !contains(calc.charAt(calc.length() - 1), operation)) {
             calc.append("×");
-            updateCalc();
+            clearWhenNextIsNumber = false;
+            updateCalcText();
         }
     }
 
     private void onDivideClick(View view) {
         if (calc.length() != 0 && !contains(calc.charAt(calc.length() - 1), operation)) {
             calc.append("/");
-            updateCalc();
+            clearWhenNextIsNumber = false;
+            updateCalcText();
         }
     }
 
     private void onPercentageClick(View view) {
         //if (calc.length() != 0 && !contains(calc.charAt(calc.length() - 1), operation)) {
         //calc.append("%");
-        //updateCalc();
+        //updateCalcText();
         //}
     }
 
     private void onEqualClick(View view) {
         if (calc.length() != 0 && !contains(calc.charAt(calc.length() - 1), operation)) {
-            evaluate();
+            equalsWasPressed = true;
+            updateResult();
+        }
+    }
+
+    private void onDotClick(View view) {
+        if (calc.length() == 0 || contains(calc.charAt(calc.length() - 1), operation) || contains(calc.charAt(calc.length() - 1), numbers)) {
+            calc.append(".");
+            updateCalcText();
         }
     }
 
@@ -290,15 +331,15 @@ public class CalculatorActivity extends AppCompatActivity {
     }
 
     private void onDeleteClick(View view) {
-        if(calc.length() > 0){
+        if (calc.length() > 0) {
             calc.deleteCharAt(calc.length() - 1);
-            updateCalc();
+            updateCalcText();
         }
     }
 
     public void changeView(View view) {
-        System.out.println("HEY");
-        switch (vf.getDisplayedChild()){
+        System.out.println("VIEW CHANGED");
+        switch (vf.getDisplayedChild()) {
             case 0:
                 vf.setDisplayedChild(1);
                 break;
@@ -310,45 +351,45 @@ public class CalculatorActivity extends AppCompatActivity {
 
 
 
-
-    static boolean contains(char c, char[] array) {
-        for (char x : array) {
-            if (x == c) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public void updateCalc() {
-        System.out.println("Calculation: " + calc);
+    public void updateCalcText() {
         calcText.setText(calc);
     }
 
-    public void evaluate() {
-        //String[] exp = calc.toString().split("(?<=[-+×/])|(?=[-+×/])");
-
+    public void updateResult() {
         String expr = calc.toString();
 
-        System.out.println("To evaluate: ");
+        System.out.println("Expression: ");
 
-        double result = EvaluateString.evaluate(expr);
+        double result = evaluate(expr);
         // To set no X.0 on Screen
         if (result % 1 == 0) {
             int newResult = (int) result;
-            resultText.setText(String.valueOf(newResult));
+            resultText.setText("= " + newResult);
+
+            if (equalsWasPressed) {
+                calc.setLength(0);
+                calc.append(newResult).toString();
+                clearWhenNextIsNumber = true;
+                equalsWasPressed = false;
+            }
         } else {
-            resultText.setText(String.valueOf(result));
+            System.out.println("result: " + result);
+            resultText.setText("= " + result);
+
+            if (equalsWasPressed) {
+                calc.setLength(0);
+                calc.append(result).toString();
+                clearWhenNextIsNumber = true;
+                equalsWasPressed = false;
+            }
         }
 
-        System.out.println(EvaluateString.evaluate("10 + 2 × 6"));
-        System.out.println(EvaluateString.evaluate("100 × 2 + 12"));
-        System.out.println(EvaluateString.evaluate("100 × ( 2 + 12 )"));
-        System.out.println(EvaluateString.evaluate("100 × ( 2 + 12 ) / 14"));
+        //System.out.println(EvaluateString.evaluate("10 + 2 × 6"));
+        //System.out.println(EvaluateString.evaluate("100 × 2 + 12"));
+        //System.out.println(EvaluateString.evaluate("100 × ( 2 + 12 )"));
+        //System.out.println(EvaluateString.evaluate("100 × ( 2 + 12 ) / 14"));
     }
-}
 
-class EvaluateString {
     public static double evaluate(String input) {
         char[] expr = input.toCharArray();
 
@@ -370,11 +411,12 @@ class EvaluateString {
                 //System.out.println("1: " + expr[i]);
 
                 // if Number.size >= 1 -> add
-                while (i < expr.length && expr[i] >= '0' && expr[i] <= '9') {
+                while (i < expr.length && expr[i] >= '0' && expr[i] <= '9' || i < expr.length && expr[i] == '.') {
                     //System.out.println("2: " + expr[i]);
                     temp.append(expr[i]);
                     i++;
                 }
+                System.out.println("2: " + temp.toString());
                 numbers.push(Double.parseDouble(temp.toString()));
                 i--; // to correct the offset again
             }
@@ -429,24 +471,44 @@ class EvaluateString {
 
     public static double calculate(char operator, double y, double x) {
         double result = 0;
+        BigDecimal x1 = new BigDecimal(x);
+        BigDecimal y1 = new BigDecimal(y);
+        BigDecimal res;
+
         switch (operator) {
             case '+':
-                result = x + y;
+                res = x1.add(y1);
+                res = res.setScale(5, BigDecimal.ROUND_HALF_UP);
+                result = res.doubleValue();
                 break;
             case '-':
-                result = x - y;
+                res = x1.subtract(y1);
+                res = res.setScale(5, BigDecimal.ROUND_HALF_UP);
+                result = res.doubleValue();
                 break;
             case '×':
-                result = x * y;
+                res = x1.multiply(y1);
+                res = res.setScale(5, BigDecimal.ROUND_HALF_UP);
+                result = res.doubleValue();
                 break;
             case '/':
                 if (y == 0) {
                     throw new ArithmeticException("Cannot divide by zero");
                 } else {
-                    result = x / y;
+                    res = x1.divide(y1, 6, BigDecimal.ROUND_HALF_UP);
+                    result = res.doubleValue();
                     break;
                 }
         }
         return result;
+    }
+
+    static boolean contains(char c, char[] array) {
+        for (char x : array) {
+            if (x == c) {
+                return true;
+            }
+        }
+        return false;
     }
 }
